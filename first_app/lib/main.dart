82d521700bd6de53acfe1fa9aed82050c3330971
CK-Arijit <arijit.naskar@cloudkaptan.com>
2025-07-23 13:34:41 +0530

    first commit

---
+++ first_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.green],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello, World!',
+              style: TextStyle(
+                fontSize: 34,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
